73725fc5418dab894f31a417edb059cdf177009d
Kim Røen <kim@kimroen.com>
2025-03-04 08:52:38 +0100

    Remove matcher extension
    The import for jest-dom/vitest already does this

--- packages/eufemia-vitest/vitest.setup.js
+++ packages/eufemia-vitest/vitest.setup.js
@@ -2,11 +2,6 @@ import '@testing-library/jest-dom/vitest'
 import { cleanup } from '@testing-library/react'
 import { afterEach } from 'vitest'
 
-// Extend matchers
-import * as matchers from '@testing-library/jest-dom/matchers'
-import { expect } from 'vitest'
-expect.extend(matchers)
-
 // Clean up after each test
 afterEach(() => {
   cleanup()
